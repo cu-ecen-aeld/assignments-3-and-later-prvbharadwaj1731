04d06749545c4893945451e573793c6be7e078f0
Pranav Bharadwaj <95208934+prvbharadwaj1731@users.noreply.github.com>
2022-08-29 20:14:12 -0600

    Changes to writer.sh for file error handling

--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -15,6 +15,22 @@ fi
 WRITE_FILE=$1
 WRITE_STR=$2
 
-# create file if it does not exist, else overwrite it (using redirection operator)
+# Check if path provided exists. Create file if it does not exist, else overwrite it (using redirection operator)
+# If file does not exist or could not be created, return exit 1. Use try-catch to implement this, using || operator
+
+{
+	if [ ! -d "$WRITE_FILE" ];
+	then
+		#create path and create file and write string to it
+		mkdir -p "$(dirname $WRITE_FILE)" && echo "$WRITE_STR" > "$WRITE_FILE"
+	else
+		echo "$WRITE_STR" > "$WRITE_FILE"
+	fi
+} || 
+{
+	echo "File cannot be created. Exiting..."
+	exit 1
+}
+
 echo "$WRITE_STR" > "$WRITE_FILE"
 
