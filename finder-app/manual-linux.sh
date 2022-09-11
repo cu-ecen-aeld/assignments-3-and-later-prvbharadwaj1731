7d71ad643b3725e12afd414ae67cc44095fcf6c5
prvbharadwaj1731 <prbh1731@colorado.edu>
2022-09-11 11:32:39 -0400

    start-qemu-terminal.sh works, but emulator doesn't recognize executable

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -97,14 +97,21 @@ echo "dependencies added"
 #editing SYSROOT and printing to check
 SYSROOT=$(${CROSS_COMPILE}gcc -print-sysroot)
 
-cp -a $SYSROOT/lib/ld-linux-aarch64.so.1 lib
-cp -a $SYSROOT/lib64/ld-2.31.so lib64
-cp -a $SYSROOT/lib64/libm.so.6 lib64
-cp -a $SYSROOT/lib64/libm-2.31.so lib64
-cp -a $SYSROOT/lib64/libresolv.so.2 lib64
-cp -a $SYSROOT/lib64/libresolv-2.31.so lib64
-cp -a $SYSROOT/lib64/libc.so.6 lib64
-cp -a $SYSROOT/lib64/libc-2.31.so lib64
+#cp -a $SYSROOT/lib/ld-linux-aarch64.so.1 lib
+#cp -a $SYSROOT/lib64/ld-2.31.so lib64
+#cp -a $SYSROOT/lib64/libm.so.6 lib64
+#cp -a $SYSROOT/lib64/libm-2.31.so lib64
+#cp -a $SYSROOT/lib64/libresolv.so.2 lib64
+#cp -a $SYSROOT/lib64/libresolv-2.31.so lib64
+#cp -a $SYSROOT/lib64/libc.so.6 lib64
+#cp -a $SYSROOT/lib64/libc-2.31.so lib64
+
+cp ${SYSROOT}/lib/ld-linux-aarch64.so.1 lib
+cp ${SYSROOT}/lib64/libm.so.6 lib64
+cp ${SYSROOT}/lib64/libresolv.so.2 lib64
+cp ${SYSROOT}/lib64/libc.so.6 lib64
+
+
 
 
 
