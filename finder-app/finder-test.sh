6acf6a7df517965f4036a43a3e553d8bac3e169a
prvbharadwaj1731 <prbh1731@colorado.edu>
2022-09-18 21:54:27 -0400

    Assignment-4-result.txt copied from qemu instance

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -54,7 +54,7 @@ do
 	writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
-OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
+OUTPUTSTRING=$(finder.sh "$WRITEDIR" "$WRITESTR")
 
 #finder-test output to be written to tmp/assignment4-result.txt
 echo ${OUTPUTSTRING} > ${OUTPUT_DIR}
