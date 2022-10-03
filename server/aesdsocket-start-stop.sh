fadbfd25bd4187a0b0bed315bf4b017ecbbca98e
prvbharadwaj1731 <prbh1731@colorado.edu>
2022-10-02 22:02:19 -0400

    Cross compiling aesdsocket for QEMU instance

--- server/aesdsocket-start-stop.sh
+++ server/aesdsocket-start-stop.sh
@@ -3,18 +3,26 @@
 # 1) https://man7.org/linux/man-pages/man8/start-stop-daemon.8.html 
 # 2) https://gist.github.com/RichardBronosky/b037de3e8763887b034298057200bd02
 
-case "$1" in 
-    start) 
-        echo "Starting aesdsocket at port 9000. Test message" 
-        start-stop-daemon -S -n aesdsocket -a /usr/bin/aesdsocket -- -d 
-        ;; 
-    stop) 
-        echo "Stopping aesdsocket at port 9000" 
-        start-stop-daemon -K -n aesdsocket 
-        ;; 
-    *) 
-        echo "How to use: $0 {start|stop}" 
-    exit 1 
-esac 
+#!/bin/sh
 
-exit 0 
+usage() {
+	echo "Command: $0 <start/stop>"
+	echo "Functionality: starts/stops the aesdsocket daemon."
+}
+
+#Check that one arguments has been provided
+if [ ! $# -eq 1 ]
+then
+	usage
+	exit 1
+fi
+
+if [ $1 = "start" ]
+then
+    echo "Starting aesdsocket"
+    start-stop-daemon -S -n aesdsocket -a /usr/bin/aesdsocket -- -d
+elif [ $1 = "stop" ]
+then
+    echo "Stopping aesdsocket"
+    start-stop-daemon -K -n aesdsocket
+fi
