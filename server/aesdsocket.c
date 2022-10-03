02a1aea03ffcd5aa430319ab0bbbee4bb80ea36f
prvbharadwaj1731 <prbh1731@colorado.edu>
2022-10-02 22:35:01 -0400

    Further changes to getaddrinfo

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -47,7 +47,7 @@ void managesocket(int socket_t) {
   syslog(LOG_INFO, "Listening on port 9000");
 
   //Create the file that will log the messages received
-  int log_fd = open(LOGFILE_PATH, O_CREAT | O_RDWR | O_TRUNC, 0766);
+  int log_fd = open(LOGFILE_PATH, O_CREAT | O_RDWR | O_TRUNC, 0666);
   if (log_fd == -1) {
     syslog(LOG_ERR, "Error creating logfile =  %s. Exiting...", strerror(errno));
     exit(-1);
@@ -61,10 +61,11 @@ void managesocket(int socket_t) {
     struct sockaddr_storage client_addr;
     socklen_t addr_size = sizeof client_addr;
 
-    int conn_fd = accept(socket_t, (struct sockaddr * ) & client_addr, & addr_size);
+    int conn_fd = accept(socket_t, (struct sockaddr *) &client_addr, &addr_size);
     if (conn_fd == -1) {
-      if (errno == EINTR)
-        break;
+      if (errno == EINTR){
+      		break;
+      }        
       else {
         syslog(LOG_ERR, "Error occured during accepting new connection = %s. Exiting...", strerror(errno));
         exit(-1);
@@ -73,14 +74,14 @@ void managesocket(int socket_t) {
     //Get IPv4 address of client
     if (client_addr.ss_family == AF_INET) {
       char addr[INET6_ADDRSTRLEN];
-      struct sockaddr_in * addr_in = (struct sockaddr_in * ) & client_addr;
-      inet_ntop(AF_INET, & (addr_in -> sin_addr), addr, INET_ADDRSTRLEN);
+      struct sockaddr_in *addr_in = (struct sockaddr_in *)&client_addr;
+      inet_ntop(AF_INET, &(addr_in->sin_addr), addr, INET_ADDRSTRLEN);
       syslog(LOG_INFO, "Accepted connection from %s", addr);
       printf("Accepted connection from %s\n", addr);
     } else if (client_addr.ss_family == AF_INET6) {
       char addr[INET6_ADDRSTRLEN];
-      struct sockaddr_in6 * addr_in6 = (struct sockaddr_in6 * ) & client_addr;
-      inet_ntop(AF_INET6, & (addr_in6 -> sin6_addr), addr, INET6_ADDRSTRLEN);
+      struct sockaddr_in6 *addr_in6 = (struct sockaddr_in6 *)&client_addr;
+      inet_ntop(AF_INET6, &(addr_in6->sin6_addr), addr, INET6_ADDRSTRLEN);
       syslog(LOG_INFO, "Accepted connection from %s", addr);
       printf("Accepted connection from %s\n", addr);
     }
@@ -90,15 +91,15 @@ void managesocket(int socket_t) {
     //How many 'BUFFER_SIZE' receive_blocks "recv_data" we have received from client till now
     int receive_blocks = 1;    
     int recv_idx = 0;
-    //Start with a size, potentially incresing it if an entire packet cannot fit into it
-    recv_data = malloc(sizeof(char) * BUFFER_SIZE * receive_blocks);
+    //Malloc space of receiving data
+    recv_data = malloc(sizeof(char)*BUFFER_SIZE*receive_blocks);
     if(!recv_data){
     	syslog(LOG_ERR, "Error occured during malloc = %s. Exiting...", strerror(errno));
     	exit(-1);
     }
     
     do {
-      recv_ret = recv(conn_fd, & recv_data[recv_idx], BUFFER_SIZE, 0);
+      recv_ret = recv(conn_fd, &recv_data[recv_idx], BUFFER_SIZE, 0);
       if (recv_ret == -1) {
         syslog(LOG_ERR, "Error occured while reading from socket = %s. Exiting...", strerror(errno));
         exit(-1);
@@ -116,7 +117,7 @@ void managesocket(int socket_t) {
 
           int write_byte_ret;
           int write_len = recv_idx;
-          char * write_ptr = recv_data;
+          char *write_ptr = recv_data;
           while (write_len != 0) {
           
             write_byte_ret = write(log_fd, write_ptr, write_len);            
@@ -146,7 +147,7 @@ void managesocket(int socket_t) {
 //          char * log_read_offset = log_read;
 //          int bytes_to_read = file_size;
 //          int bytes_read_count;
-          while (bytes_to_read != 0) {
+          while (bytes_to_read){
           
           int bytes_sent_count = 0;
           int bytes_read_count = read(log_fd, log_read, BUFFER_SIZE);
@@ -184,9 +185,11 @@ void managesocket(int socket_t) {
          recv_idx = 0;
         }
         //Realloc the array if it got full without an '\n'
-        else if (recv_idx == (BUFFER_SIZE * receive_blocks)) {
+        else if (recv_idx == (BUFFER_SIZE*receive_blocks)) {
+        
           receive_blocks++;
-          recv_data = realloc(recv_data, sizeof(char) * BUFFER_SIZE * receive_blocks);
+          
+          recv_data = realloc(recv_data, sizeof(char)*BUFFER_SIZE*receive_blocks);
           if(!recv_data){
           	syslog(LOG_ERR, "Error occured during realloc = %s. Exiting...", strerror(errno));
           	exit(-1);
@@ -201,13 +204,13 @@ void managesocket(int socket_t) {
 
     if (client_addr.ss_family == AF_INET) {
       char addr[INET6_ADDRSTRLEN];
-      struct sockaddr_in * addr_in = (struct sockaddr_in * ) & client_addr;
-      inet_ntop(AF_INET, & (addr_in -> sin_addr), addr, INET_ADDRSTRLEN);
+      struct sockaddr_in *addr_in = (struct sockaddr_in *)&client_addr;
+      inet_ntop(AF_INET, &(addr_in->sin_addr), addr, INET_ADDRSTRLEN);
       syslog(LOG_INFO, "Closed connection from %s", addr);
     } else if (client_addr.ss_family == AF_INET6) {
       char addr[INET6_ADDRSTRLEN];
-      struct sockaddr_in6 * addr_in6 = (struct sockaddr_in6 * ) & client_addr;
-      inet_ntop(AF_INET6, & (addr_in6 -> sin6_addr), addr, INET6_ADDRSTRLEN);
+      struct sockaddr_in6 *addr_in6 = (struct sockaddr_in6 *)&client_addr;
+      inet_ntop(AF_INET6, &(addr_in6->sin6_addr), addr, INET6_ADDRSTRLEN);
       syslog(LOG_INFO, "Closed connection from %s", addr);
     }
   }
@@ -226,6 +229,7 @@ void managesocket(int socket_t) {
     syslog(LOG_ERR, "Error occured during deleting log file = %s. Exiting...", strerror(errno));
     exit(-1);
   }
+  return;
 }
 
 int main(int argc, char ** argv) {
@@ -238,7 +242,7 @@ int main(int argc, char ** argv) {
   signal_action.sa_handler = signal_handler;
   signal_action.sa_flags = 0;
   sigset_t empty;
-  if (sigemptyset( & empty) == -1) {
+  if (sigemptyset(&empty) == -1) {
     syslog(LOG_ERR, "Error occured during setting empty set = %s. Exiting...", strerror(errno));
     exit(-1);
   }
@@ -246,11 +250,11 @@ int main(int argc, char ** argv) {
 
   //Set handler for the signals SIGINT and SIGTERM
 
-  if (sigaction(SIGTERM, & signal_action, NULL) == -1) {
+  if (sigaction(SIGTERM, &signal_action, NULL) == -1) {
     syslog(LOG_ERR, "Error occured during setting handler for SIGTERM = %s. Exiting...", strerror(errno));
     exit(-1);
   }
-  if (sigaction(SIGINT, & signal_action, NULL) == -1) {
+  if (sigaction(SIGINT, &signal_action, NULL) == -1) {
     syslog(LOG_ERR, "Error occured during setting handler for SIGINT = %s. Exiting...", strerror(errno));
     exit(-1);
   }
@@ -259,7 +263,7 @@ int main(int argc, char ** argv) {
   struct addrinfo hints;
   struct addrinfo *result;
   memset(&hints, 0, sizeof(struct addrinfo));
-  hints.ai_family = AF_INET; //chose any protocol as system sees fit
+  hints.ai_family = AI_PASSIVE; //chose any protocol as system sees fit
   hints.ai_socktype = SOCK_STREAM; //chose TCP
   if (getaddrinfo(NULL, "9000", &hints, &result) != 0) {
     syslog(LOG_ERR, "Error occured during socket setup. Exiting...");
@@ -282,7 +286,7 @@ int main(int argc, char ** argv) {
 
 
   //bind socket
-  if (bind(socket_t, result -> ai_addr, result -> ai_addrlen) == -1) {
+  if (bind(socket_t, result->ai_addr, result->ai_addrlen) == -1) {
     syslog(LOG_ERR, "Error occured during socket binding = %s. Exiting...", strerror(errno));
     exit(-1);
   }
@@ -295,7 +299,8 @@ int main(int argc, char ** argv) {
   if (argc == 1) {
     syslog(LOG_INFO, "No argument provided. Running in non-daemon mode.");
     managesocket(socket_t);
-  } else if (argc == 2) {
+  } 
+  else if (argc == 2) {
 
     //check input argument
     if (strcmp(argv[1], "-d") != 0) {
@@ -312,7 +317,8 @@ int main(int argc, char ** argv) {
     if (fork_ret == -1) {
       syslog(LOG_ERR, "Error occured during fork() = %s. Exiting...", strerror(errno));
       exit(-1);
-    } else if (fork_ret == 0) {
+    } 
+    else if (fork_ret == 0) {
       //In child now            
       //remove terminal associated with child
       if (setsid() == -1) {
@@ -324,15 +330,14 @@ int main(int argc, char ** argv) {
         syslog(LOG_ERR, "Error occured during setting directory to root = %s. Exiting...", strerror(errno));
         exit(-1);
       }
-
-      //redirect stdout
-      dup(0);
+      
       managesocket(socket_t);
       exit(0);
     }
     //Exit parent process to complete daemon creation
     exit(0);
-  } else {
+  } 
+  else {
     syslog(LOG_ERR, "Error occured with invalid arguments");
     syslog(LOG_ERR, "to use in daemon mode, use ./aesdsocket -d");
     exit(-1);
